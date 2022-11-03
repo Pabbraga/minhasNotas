98c9312892f070998ae300cce8bb20d494f50284
Pabraga <103124116+Pabbraga@users.noreply.github.com>
2022-11-03 10:33:45 -0300

    organização de pastas

---
+++ script/login.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once '../components/post.php';
+require_once '../components/user.php';
+require_once '../components/session.php';
+
+$session = new Session();
+$post = new Post();
+$user = new User();
+
+if(isset($_POST['email']) && isset($_POST['password'])) {
+    $email = $post->get('email');
+    $pass = $post->get('password');
+
+    if($email == $user->getEmail() && $pass == $user->getPassword()) {
+        $_SESSION['user'] = $user->getEmail();
+        include '../library/main.html';
+    } else {
+        echo 'dados incorretos e/ou inválidos!';
+    }
+} else {
+    echo 'dados não foram preenchidos!';
+}
