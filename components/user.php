1cc82053ee8c935a1bb95cc28dfd58118965d7eb
Pabraga <103124116+Pabbraga@users.noreply.github.com>
2022-11-25 22:46:07 -0300

    correções sobre conexão com o banco

--- components/user.php
+++ components/user.php
@@ -2,35 +2,39 @@
 
 class User {
 
+    private $name;
+    private $cpf;
     private $email;
     private $password;
-    private $name;
-    private $age;
-    private $cpf;
     private $picture;
     
-    public function getEmail() {
-        return $this->email;
-    }
-    
-    public function getPassword() {
-        return $this->password;
-    }
 
     public function getName() {
         return $this->name;
-    }
-
-    public function getAge() {
-        return $this->age;
     }
 
     public function getCpf() {
         return $this->cpf;
     }
 
+    public function getEmail() {
+        return $this->email;
+    }
+
+    public function getPassword() {
+        return $this->password;
+    }
+
     public function getPicture(){
         return $this->picture;
+    }
+
+    public function setName($v) {
+        $this->name = $v;
+    }
+
+    public function setCpf($v) {
+        $this->cpf = $v;
     }
 
     public function setEmail($v) {
@@ -39,18 +43,6 @@ class User {
     
     public function setPassword($v) {
         $this->password = $v;
-    }
-
-    public function setName($v) {
-        $this->name = $v;
-    }
-
-    public function setAge($v) {
-        $this->age = $v;
-    }
-
-    public function setCpf($v) {
-        $this->cpf = $v;
     }
 
     public function setPicture($v){
--- script/login.php
+++ script/login.php
@@ -9,24 +9,23 @@ require './config.php';
 $session = new Session();
 $post = new Post();
 $user = new User();
-$dt = new DataBase($_config);
+$db = new DataBase($_config);
 
 if(isset($_POST['email']) && isset($_POST['password']) && $_POST['email'] != '' && $_POST['password'] != '') {
     $email = $post->get('email');
     $pass = $post->get('password');
 
-    $usersData = $dt->selectData('user');
+    $usersData = $db->selectData('user');
     foreach($usersData as $Data) {
         $userData = $Data;
+        if($email == $userData['email'] && $pass == $userData['password']) {
+            $user->setName($userData['name']);
+            $user->setCpf($userData['cpf']);
+            $user->setEmail($userData['email']);
+            $user->setPassword($userData['password']);
+            $user->setPicture($userData['picture']);
+        }
     }
-    if($email == $Data['email'] && $pass == $Data['password']) {
-        $user->setName($userData['name']);
-        $user->setCpf($userData['cpf']);
-        $user->setEmail($userData['email']);
-        $user->setPassword($userData['password']);
-        $user->setPicture($userData['picture']);
-    }
-    
     if($email == $user->getEmail() && $pass == $user->getPassword()) {
         $_SESSION['user'] = $user->getEmail();
         include '../pages/main.php';
